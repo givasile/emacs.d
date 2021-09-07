32ed061080b5705319f75b567e6847fe813c5dca
Vasilis Gkolemis <ntipakos@gmail.com>
2021-09-07 14:51:07 +0300

    added mu3e and nano configuration

--- givasile/add-remote-packages.el
+++ givasile/add-remote-packages.el
@@ -1,24 +1,24 @@
-;; INSTALL PACKAGES
+;; REMOTE PACKAGES TO BE INSTALLED APART
+;; FROM THE ONES INSTALLED BY DEFAULT FROM
+;; https://github.com/purcell/emacs.d
 ;; --------------------------------------
 
+;; general packages
 (defvar generalPackages
   '(
-    use-package               ;; adds use-package macro for configuring packages in a tidy way
-    simple-httpd              ;; simple emacs web server
-    ;; yasnippet                 ;; supports auto-complete snippets
-    ;; yasnippet-snippets        ;; list of common snippets
-    ;; company                   ;; auto-completion tool
-    ;; projectile                ;; offers project-level manipulation (find etc)
-    ;; magit                     ;; git inside emacs
-    markdown-mode             ;; edit markdowns from emacs
-    csv-mode                  ;; edit csv from emacs
-    impatient-mode            ;; live-preview of html in the browser (built on top of simple-httpd)
-    flycheck                  ;; on-the-fly checker (uses many backends, depending on the language)
-    ;; nord-theme                ;; nice theme from https://github.com/arcticicestudio/nord-emacs
-    ;; material-theme            ;; add the material theme
+    use-package ;; adds use-package macro for configuring packages in a tidy way
+    simple-httpd  ;; simple emacs web server
+    markdown-mode ;; edit markdowns from emacs
+    csv-mode      ;; edit csv from emacs
+    impatient-mode ;; live-preview of html in the browser (built on top of simple-httpd)
+    flycheck ;; on-the-fly checker (uses many backends, depending on the language)
+    calfw    ;; calendar view
+    calfw-org        ;;
+    org-super-agenda ;; better-view of the typical agenda
+    material-theme   ;; nano related - add the material theme
     ))
 
-;; TODO add descriptions
+;; python packages
 (defvar pythonPackages
   '(better-defaults 
     ein
@@ -26,6 +26,7 @@
     py-autopep8
     ))
 
+;; latex packages
 (defvar latexPackages
   '(
     auctex ;; writing and formatting tex documents
@@ -36,6 +37,32 @@
 
 ;; install each package only if not installed already
 (mapc #'(lambda (package)
-    (unless (package-installed-p package)
-      (package-install package)))
+          (unless (package-installed-p package)
+            (package-install package)
+            )
+          )
       allPackages)
+
+;; Install straight.el
+(defvar bootstrap-version)
+(let ((bootstrap-file
+       (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
+      (bootstrap-version 5))
+  (unless (file-exists-p bootstrap-file)
+    (with-current-buffer
+        (url-retrieve-synchronously
+         "https://raw.githubusercontent.com/raxod502/straight.el/develop/install.el"
+         'silent 'inhibit-cookies)
+      (goto-char (point-max))
+      (eval-print-last-sexp)))
+  (load bootstrap-file nil 'nomessage))
+
+;; install packages on the fly with straight.el
+(straight-use-package
+ '(nano-emacs :type git :host github :repo "rougier/nano-emacs"))
+
+(straight-use-package
+ '(mu4e-thread-folding :type git :host github :repo "rougier/mu4e-thread-folding"))
+
+(straight-use-package
+ '(mu4e-dashboard :type git :host github :repo "rougier/mu4e-dashboard"))
--- givasile/config-org.el
+++ givasile/config-org.el
@@ -1,2 +1,135 @@
 ;; defines the directory where the agenda files are
 (setq org-agenda-files (list "~/org/"))
+
+
+;;store org-mode links to emails
+(require 'org-mu4e)
+;;store link to message if in header view, not to header query
+(setq org-mu4e-link-query-in-headers-mode nil)
+
+;; template for storing emails as todo tasks
+(setq org-capture-templates
+      '(("t" "todo" entry (file+headline "~/org/captures.org" "Tasks")
+         "* TODO [#A] %?\nSCHEDULED: %(org-insert-time-stamp (org-read-date nil t \"+0d\"))\n%a\n")))
+
+
+;; set the .org file, that all captured notes are saved to
+(setq org-default-notes-file (concat org-directory "/captures.org"))
+
+
+;; add calendar package
+(require 'calfw)
+(require 'calfw-org)
+(defalias 'ca 'cfw:open-org-calendar)
+
+
+
+;; org-super-agenda configuration
+(  )
+
+(use-package org-super-agenda
+  :ensure t
+  :config
+  (org-super-agenda-mode)
+  )
+
+(setq org-agenda-custom-commands
+      '(("c" "Super Agenda" agenda
+         (org-super-agenda-mode)
+         ((org-super-agenda-groups
+           '(
+             (:name "Next Items"
+                    :time-grid t
+                    :tag ("NEXT" "outbox"))
+             (:name "Important"
+                    :priority "A")
+             (:name "Today"
+                    :time-grid t
+                    :scheduled today)
+             (:name "Quick Picks"
+                    :effort < 00:30 ; ← that one doesn't seem to work, no?
+                    )
+             (:priority<= "B"
+                          :order 1)
+             )))
+         (org-agenda nil "a"))))
+
+(setq super-agenda-groups1
+      '((:log t)  ; Automatically named "Log"
+        (:name "Schedule"
+               :time-grid t)
+        (:name "Today"
+               :scheduled today)
+        (:habit t)
+        (:name "Due today"
+               :deadline today)
+        (:name "Overdue"
+               :deadline past)
+        (:name "Due soon"
+               :deadline future)
+        (:name "Unimportant"
+               :todo ("SOMEDAY" "MAYBE" "CHECK" "TO-READ" "TO-WATCH")
+               :order 100)
+        (:name "Waiting..."
+               :todo "WAITING"
+               :order 98)
+        (:name "Scheduled earlier"
+               :scheduled past))
+      )
+(setq super-agenda-groups
+      '(;; Each group has an implicit boolean OR operator between its selectors.
+        (:name "Today"            ; Optionally specify section name
+               :time-grid t       ; Items that appear on the time grid
+               )
+        (:name "DEADLINES" :deadline t :order 1)
+        (:name "Focus" :tag "Focus" :order 2 :face (:append t :weight bold))
+        (:name "Important" :priority "A" :order 4)
+        (:priority<= "B"
+                     ;; Show this section after "Today" and "Important", because
+                     ;; their order is unspecified, defaulting to 0. Sections
+                     ;; are displayed lowest-number-first.
+                     :order 6)
+                                        ;        (:name "Habits" :habit t :order 3)
+        (:name "Shopping" :tag "Besorgung" :order 8)
+        ;; Boolean AND group matches items that match all subgroups
+        ;;  :and (:tag "shopping" :tag "@town")
+        ;; Multiple args given in list with implicit OR
+        ;;  :tag ("food" "dinner"))
+        ;;  :habit t
+        ;;  :tag "personal")
+        (:name "Started" :todo "STARTED" :order 10)
+        ;;(:name "Space-related (non-moon-or-planet-related)"
+        ;;       ;; Regexps match case-insensitively on the entire entry
+        ;;       :and (:regexp ("space" "NASA")
+        ;;                     ;; Boolean NOT also has implicit OR between selectors
+        ;;                     :not (:regexp "moon" :tag "planet")))
+        (:name "BWG" :tag "@BWG" :order 16)
+        (:todo "WAITING" :order 18)     ; Set order of this section
+        (:name "read" :tag "2read" :order 22)
+        ;; Groups supply their own section names when none are given
+        (:todo ("SOMEDAY" "WATCHING")
+               ;; Show this group at the end of the agenda (since it has the
+               ;; highest number). If you specified this group last, items
+               ;; with these todo keywords that e.g. have priority A would be
+               ;; displayed in that group instead, because items are grouped
+               ;; out in the order the groups are listed.
+               :order 25)
+        (:name "reward"
+               :tag ("reward" "lp")
+               :order 100
+               )
+
+        ;; After the last group, the agenda will display items that didn't
+        ;; match any of these groups, with the default order position of 99
+        )
+      )
+
+(defun my-super-agenda()
+  "generates my super-agenda"
+  (interactive)
+  (org-super-agenda-mode)
+  (let
+      ((org-super-agenda-groups super-agenda-groups1))
+    (org-agenda nil "a")
+    )
+  )
